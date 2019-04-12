b611f286274e7277b7fd0ca480aae8277aa2002e
randy brown <randy@beingbrown.net>
2019-04-11 21:34:54 -0500

    provide an api for building routers
    the makeRouter function is tightly coupled to express and so it is
    declared in the express scope. If the server + router behavior were
    an abstraction for discussing services, it might be worth it to
    extract both.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var Router = express.Router;
+
+const makeRouter = (behaviors, asyncManager) => {
+  const router = Router();
+  behaviors.forEach(({endpoint, method, behavior}) => {
+    router[method](endpoint, asyncManager(behavior));
+  });
+  return router;
+}
+
 var app = express();
 
 app.use(logger('dev'));
