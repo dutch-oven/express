ef7345061f7d313b8e5a5db8c721df3abcc92764
randy brown <randy@beingbrown.net>
2019-05-22 21:09:14 -0500

    provide a series of security middleware for express
    according to express's documentation, helmet will protect against
    many security issues.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express, { Router } from 'express';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
+import helmet from 'helmet';
 
 const makeRouter = (behaviors, asyncManager) => {
   const router = Router();
@@ -12,6 +13,7 @@ const makeRouter = (behaviors, asyncManager) => {
 
 const app = express();
 
+app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
