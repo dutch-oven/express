0ef7b7d0f7689d06b3adfa2b1d1bbc40d6f16f31
randy brown <randy@beingbrown.net>
2019-05-25 21:49:50 -0500

    leverage the logger util to provide logging
    in order to get unified logging on all express services, the same
    logger will be leveraged. This will also support migrating to new
    logging structures independent of app build.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import csrf from 'csurf';
+import logger from './util/logger';
 
 const makeRouter = (behaviors, asyncManager) => {
   const router = Router();
@@ -16,7 +17,7 @@ const makeApp = config => {
   const app = express();
 
   app.use(helmet());
-  app.use(morgan('dev'));
+  app.use(morgan('combined', { stream: logger.stream }));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
