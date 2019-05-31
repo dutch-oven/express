dc2ce1e360e62f936d8153f538a129a8b7a63bfb
randy brown <randy@beingbrown.net>
2019-05-31 09:39:30 -0500

    provide an interface for uniform messages from createError
    this will need to be unwrapped a little, as logging should ideally
    provide this as a nested JSON object, rather than a string, if the
    json format is leveraged.

--- src/util/errorHandler.js
+++ src/util/errorHandler.js
@@ -1,5 +1,9 @@
-export const errorHandler = logger => (error, req, res, next) => {
+import createError from 'http-errors'
+
+const errorHandler = logger => (error, req, res, next) => {
   logger.error (error);
   const { status, message } = createError(error.status || 500);
   res.status(status).json({ message });
 };
+
+export default errorHandler;
