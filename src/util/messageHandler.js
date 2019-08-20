6575c6c7ba6d031f99ed7f30b184d2b0b59a731e
randy brown <randy@beingbrown.net>
2019-08-19 22:02:47 -0500

    provide a read write interface to the message broker
    at present, this establishes a persistent connection with the broker
    and sends messages. In the future, it may be better to defer close and
    kill the connection when nothing happens.

--- src/util/messageHandler.js
+++ src/util/messageHandler.js
@@ -2,32 +2,53 @@ import amqp from 'amqplib';
 
 let conn, ch, uri;
 
-const establishConnection = connString => {
+// const defer = (callback, {timeout} = {timeout: 500}) => {
+//   let id;
+//   return (...args) => {
+//     if(id) clearTimeout(id);
+//     id = setTimeout(() => callback(...args), timeout);
+//   }
+// }
+
+// const close = defer(() => {
+//   conn.close();
+//   conn = undefined;
+//   ch = undefined;
+// }, {timeout: 5000});
+
+const establishConnection = async connString => {
   uri = connString;
-  amqp.connect(uri, (err, connection) => {
-    if (err) throw err;
-    conn = connection;
-  });
+  if(!conn) conn = await amqp.connect(uri);
+  // close();
+  return conn;
 }
 
-const createChannel = conn => {
-  conn.createChannel((err, channel) => {
-    if(err) throw err;
-
-    channel.assertQueue(queue, {
-      durable: false
-    });
-
-    ch = channel;
+const createChannel = async (connection, queue) => {
+  if(!connection) await establishConnection(uri);
+  if(!ch) ch = await conn.createChannel();
+  ch.assertQueue(queue, {
+    durable: false
   });
+  // close();
+  return ch;
 };
 
-const write = (msg, queue) => {
-  if(!ch) createChannel(establishConnection(uri));
+const write = async (queue, msg) => {
+  if(!ch) await createChannel(establishConnection(uri), queue);
   ch.sendToQueue(queue, Buffer.from(msg));
+  // close();
 };
 
-const read = (handler, queue) => {
-  if(!ch) createChannel(establishConnection(uri));
+const read = async (queue, handler) => {
+  if(!ch) await createChannel(establishConnection(uri), queue);
   ch.consume(queue, handler, { noAck: true });
+  // close();
+};
+
+export default ({uri}) => {
+  establishConnection(uri);
+  return {
+    read,
+    write
+  }
 };
