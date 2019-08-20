f7c020cb062506a58ed1127401ad7b27b052de9b
randy brown <randy@beingbrown.net>
2019-08-19 22:05:15 -0500

    remove dead code
    this code isn't being used.

--- src/util/messageHandler.js
+++ src/util/messageHandler.js
@@ -2,24 +2,9 @@ import amqp from 'amqplib';
 
 let conn, ch, uri;
 
-// const defer = (callback, {timeout} = {timeout: 500}) => {
-//   let id;
-//   return (...args) => {
-//     if(id) clearTimeout(id);
-//     id = setTimeout(() => callback(...args), timeout);
-//   }
-// }
-
-// const close = defer(() => {
-//   conn.close();
-//   conn = undefined;
-//   ch = undefined;
-// }, {timeout: 5000});
-
 const establishConnection = async connString => {
   uri = connString;
   if(!conn) conn = await amqp.connect(uri);
-  // close();
   return conn;
 }
 
@@ -29,20 +14,17 @@ const createChannel = async (connection, queue) => {
   ch.assertQueue(queue, {
     durable: false
   });
-  // close();
   return ch;
 };
 
 const write = async (queue, msg) => {
   if(!ch) await createChannel(establishConnection(uri), queue);
   ch.sendToQueue(queue, Buffer.from(msg));
-  // close();
 };
 
 const read = async (queue, handler) => {
   if(!ch) await createChannel(establishConnection(uri), queue);
   ch.consume(queue, handler, { noAck: true });
-  // close();
 };
 
 export default ({uri}) => {
